efa55561154060606b3b1ab6fd96c84f6ca8aee0
Pablo Sanchez <pablo.sanchez@aranova.es>
2018-11-27 17:01:27 +0100

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,20 @@
+
+
+// Guardar en el cache dinámico
+function actualizaCacheDinamico( dynamicCache, req, res ) {
+
+    if (res.ok ) {
+
+        return caches.open( dynamicCache ).then( cache => {
+
+            cache.put( req, res.clone() );
+
+            return res.clone();
+
+        });
+
+    } else {
+        return res;
+    }
+
+};
